f69721e5be897f2e7d82c707d747de2dac5bc75c
jruizmf <jruizmf@gmail.com>
2023-07-21 09:13:27 -0700

    Adding dashboard and compose options

--- src/app/pages/dashboard/dashboard-compose-options/dashboard-compose-option.module.ts
+++ src/app/pages/dashboard/dashboard-compose-options/dashboard-compose-option.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { productsDB } from 'src/app/shared/data/products';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
-import { DashboardComposeOptionsComponent } from './dashboard-compose-options.component';
 import { DashboardComposeOptionsRoutingModule } from './dashboard-compose-options-routing.module';
+import { DashboardComposeOptionsComponent } from './dashboard-compose-options.component';
 
 
 
