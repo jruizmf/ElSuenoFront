8e5ec305f469b2b3e38d415d44b6dbcb39873015
jruizmf <jruizmf@gmail.com>
2023-07-31 11:21:44 -0700

    Starting to fix cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -11,6 +11,7 @@ export class CartComponent {
   cartItems: any[] = [];
   orders: any[] = [];
   itemQuantity: number[] = [];
+  itemTotal: number[] = [];
   total: any = {
     price:0,
     discount:0,
@@ -23,8 +24,7 @@ export class CartComponent {
       let cart = JSON.parse(localStorage.getItem('cart') || '{}');
       console.log(cart)
       cart.items.forEach((item: any, index:number) => {
-        console.log(item)
-        console.log(index)
+        this.itemTotal[index] = item.total
         this.itemQuantity[index] = 1;
         this.total.price = this.total.price + item.total
         this.total.tax = (this.total.price * .15);
@@ -37,31 +37,35 @@ export class CartComponent {
   ngOnInit(): void {
    
   }
-  calculateTotals(event: any){
+  calculateTotals(event: any, i:number){
     console.log(event)
+    console.log(event)
+    let cart = JSON.parse(localStorage.getItem('cart') || '{}');
+      console.log(cart)
+      this.itemQuantity[i] = event
+      this.itemTotal[i] = this.cartItems[i].total * this.itemQuantity[i]
+    cart.items.forEach((item: any, index:number) => {
+      
+      this.itemQuantity[index] = 1;
+      this.total.price = this.total.price + (item.total * this.itemQuantity[index])
+      this.total.tax = (this.total.price * .15);
+      this.total.total = this.total.price + this.total.tax
+    });
   }
   removeItem(i:number){
     let cartString  = localStorage.getItem('cart')
     if(cartString != null){
-      let cart = JSON.parse(localStorage.getItem('cart') || '{}');
-
-
       this.cartItems.splice(i, 1);
       if(this.cartItems.length == 0){
         Swal.fire("Remove Item", "There is no items in cart, redirecting...", "success")
-        
         localStorage.removeItem('cart')
         this.cartItems = []
         this.router.navigate(['products']);
-
       }
-      console.log(this.cartItems)
+
       localStorage.setItem('cart', JSON.stringify(
         {items: this.cartItems}
       ));
     }
-    // localStorage.removeItem('cart')
-
-    // this.cartItems = []
   }
 }
